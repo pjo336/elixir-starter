8ec24141521d8202830299607f909d3dfc76a8d5
Peter Johnston <peter.johnston@coderian.dev>
2022-05-05 21:26:40 -0600

    Fix tests

--- test/myapp_web/controllers/user_registration_controller_test.exs
+++ test/myapp_web/controllers/user_registration_controller_test.exs
@@ -14,7 +14,7 @@ defmodule MyAppWeb.UserRegistrationControllerTest do
 
     test "redirects if already logged in", %{conn: conn} do
       conn = conn |> log_in_user(insert(:user)) |> get(Routes.user_registration_path(conn, :new))
-      assert redirected_to(conn) == "/"
+      assert redirected_to(conn) == "/dashboard"
     end
   end
 
@@ -29,7 +29,7 @@ defmodule MyAppWeb.UserRegistrationControllerTest do
         })
 
       assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+      assert redirected_to(conn) == "/dashboard"
 
       # Now do a logged in request and assert on the menu
       conn = get(conn, "/dashboard")
